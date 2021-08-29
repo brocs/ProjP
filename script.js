20ff3a1f88aa1476abba923a072bf66670090908
BROX <fjbroca2014@gmail.com>
2021-08-30 01:18:26 +0800

    New Update

--- script.js
+++ script.js
@@ -17,6 +17,39 @@ $(document).ready(function(){
         backSpeed: 60,
         loop: true,
     });
+
+    $('.menu-btn').click(function(){
+        $('.navbar .menu').toggleClass("active");
+        $('.menu-btn i').toggleClass("active");
+    });
+    var typed = new Typed (".typing-1", {
+        strings: ["BROX", "FILBEN"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true,
+    });
+    // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+            0:{
+                items: 1,
+                nav: false
+            },
+            600:{
+                items: 2,
+                nav: false
+            },
+            1000:{
+                items: 3,
+                nav: false
+            }
+        }
+    });
 });
 
 
